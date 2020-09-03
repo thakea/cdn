3ac76e60d40abcc870281ff6776b94372daea7f7
thakea <70716399+thakea@users.noreply.github.com>
2020-09-03 21:38:47 +0300

    Update site_cart.js

--- site_cart.js
+++ site_cart.js
@@ -1,1 +1,2 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script><script>$(document).ready(function(){document.getElementById("addproduct").submit();setTimeout(function(){document.location.href = 'https://thakea.com/cart';},250);});</script>
+window.jQuery || document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"><\/script>');
+$(document).ready(function(){document.getElementById("addproduct").submit();setTimeout(function(){document.location.href = 'https://thakea.com/cart';},250);});</script>
