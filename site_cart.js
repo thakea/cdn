477823e4cd1e3689815192c4be33b232ae8a0b8f
thakea <70716399+thakea@users.noreply.github.com>
2020-09-04 21:27:24 +0300

    Update site_cart.js

--- site_cart.js
+++ site_cart.js
@@ -1,2 +1,1 @@
-console.log(document.querySelectorAll("[data-salla-click-event='cart::adding-item']")[0]);
 document.addEventListener("DOMContentLoaded", function(){ document.querySelectorAll("[data-salla-click-event='cart::adding-item']")[0].addEventListener("click",function(){setTimeout(function(){document.location.href='https://thakea.com/cart';},250);});});
