2716b14c617d04837967aa6f2465a2528c2a2735
thakea <70716399+thakea@users.noreply.github.com>
2020-09-04 13:57:13 +0300

    Update site_cart.js

--- site_cart.js
+++ site_cart.js
@@ -1,2 +1,1 @@
-window.jQuery || document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"><\/script>');
-$(document).ready(function(){document.getElementById("addproduct").submit();setTimeout(function(){document.location.href = 'https://thakea.com/cart';},250);});</script>
+
