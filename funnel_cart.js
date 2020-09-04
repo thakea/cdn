03556065c3196a7a744b2581cd8dd84c7bb81adc
thakea <70716399+thakea@users.noreply.github.com>
2020-09-04 13:48:14 +0300

    Update funnel_cart.js

--- funnel_cart.js
+++ funnel_cart.js
@@ -1,3 +1,1 @@
-var getUrl = window.location;
-var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-document.getElementById("addproduct").submit();setTimeout(function(){document.location.href = baseUrl + '/cart';},250);
+document.getElementById("addproduct").submit();setTimeout(function(){document.location.href = 'https://thakea.com/cart';},250);
