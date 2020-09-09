740b4d27aa194f1018ad3f82c5ce93ba09b84fed
thakea <70716399+thakea@users.noreply.github.com>
2020-09-09 11:38:12 +0300

    Update offer1.js

--- offer1.js
+++ offer1.js
@@ -1,5 +1,5 @@
 window.addEventListener('popstate', function (event) {
-if(document.location.href === 'file:///home/toshiba/test.html#?offer1'){
+if(document.location.href === 'https://offer.thakea.com/#?offer1'){
     window.location.replace('https://thakea.com/XPmGqV');
 }
 
