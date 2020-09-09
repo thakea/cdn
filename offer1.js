b9edc1a415b7cf153173e0d8cd40fa1117e4d20e
thakea <70716399+thakea@users.noreply.github.com>
2020-09-09 11:55:04 +0300

    Update offer1.js

--- offer1.js
+++ offer1.js
@@ -2,5 +2,5 @@
   var els = document.getElementsByClassName('btn');
   for (var i = 0; i < els.length; i++) {
     els[i].id=i;
-    els[i].addEventListener('click', function () { console.log(this.onclick); });
+    els[i].addEventListener('click', function () { console.log(this.onclick[0]); });
   }
