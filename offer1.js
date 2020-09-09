915c09406e4c611fab1a2738971e0e018f62d014
thakea <70716399+thakea@users.noreply.github.com>
2020-09-09 11:03:40 +0300

    Update offer1.js

--- offer1.js
+++ offer1.js
@@ -1,10 +1,6 @@
-if( window.location.href == 'https://offer.thakea.com/#?offer1'){
-  window.location.replace('https://thakea.com/XPmGqV');
-}
-function locationHashChanged() {
-  if (location.hash === '#?offer1') {
-    console.log("You're visiting Offer1!");
-  }
-}
 
-window.onhashchange = locationHashChanged;
+window.onhashchange = function locationHashChanged() {
+  if (location.hash === '#?offer1') {
+    window.location.replace('https://thakea.com/XPmGqV');
+  }
+};
