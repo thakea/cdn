2b196aeb75fe08d01acb239c9fea1398d8de92d6
thakea <70716399+thakea@users.noreply.github.com>
2020-09-09 13:39:24 +0300

    Update and rename offer1.js to offer_redirect.js

--- offer1.js
+++
@@ -1,6 +0,0 @@
-
-  var els = document.getElementsByClassName('btn');
-  for (var i = 0; i < els.length; i++) {
-    els[i].id=i;
-    els[i].addEventListener('click', function () { console.log(this); console.log(this.getAttribute('onclick');); });
-  }
---
+++ offer_redirect.js
@@ -0,0 +1,15 @@
+document.getElementById('m727').querySelectorAll(".btn")[0].addEventListener("click", function() {
+   console.log("m727 (offer1) was clicked");
+   window.location.replace('https://thakea.com/XPmGqV');
+});
+
+document.getElementById('m731').querySelectorAll(".btn")[0].addEventListener("click", function() {
+   console.log("m731 (offer2) was clicked");
+   window.location.replace('https://thakea.com/PlPwOG');
+});
+
+
+document.getElementById('m732').querySelectorAll(".btn")[0].addEventListener("click", function() {
+   console.log("m732 (offer3) was clicked");
+   window.location.replace('https://thakea.com/rxWKqQ');
+});
