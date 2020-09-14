ee44cf4b74cc96c839a409016734c3894e809578
thakea <70716399+thakea@users.noreply.github.com>
2020-09-14 09:59:55 +0300

    Update offer_redirect.js

--- offer_redirect.js
+++ offer_redirect.js
@@ -1,15 +1,18 @@
 document.getElementById('m727').querySelectorAll(".btn")[0].addEventListener("click", function() {
    console.log("m727 (offer1) was clicked");
+   snaptr('track', 'CUSTOM_EVENT_2', {'offer': 'offer1'});  
    window.location.replace('https://thakea.com/XPmGqV');
 });
 
 document.getElementById('m731').querySelectorAll(".btn")[0].addEventListener("click", function() {
    console.log("m731 (offer2) was clicked");
+   snaptr('track', 'CUSTOM_EVENT_2', {'offer': 'offer2'});  
    window.location.replace('https://thakea.com/PlPwOG');
 });
 
 
 document.getElementById('m732').querySelectorAll(".btn")[0].addEventListener("click", function() {
    console.log("m732 (offer3) was clicked");
+   snaptr('track', 'CUSTOM_EVENT_2', {'offer': 'offer3'});     
    window.location.replace('https://thakea.com/rxWKqQ');
 });
