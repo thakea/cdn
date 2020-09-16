d67ebcb3773ba3d008e983efae64835abdffac1c
thakea <70716399+thakea@users.noreply.github.com>
2020-09-16 16:10:45 +0300

    Update snap_custom_events.js

--- snap_custom_events.js
+++ snap_custom_events.js
@@ -1,15 +1,15 @@
 if(window.location.href=='https://offer.thakea.com/upsell')
    snaptr('track', 'CUSTOM_EVENT_1', {'upsell': 1});  
 
-if(window.location.href=='https://offer.thakea.com/upsell_bundle')
-   snaptr('track', 'CUSTOM_EVENT_1', {'upsell_bundle': 1});  
+if(window.location.href=='https://offer.thakea.com/upsellbundle')
+   snaptr('track', 'CUSTOM_EVENT_1', {'upsellbundle': 1});  
 
 if(window.location.href=='https://offer.thakea.com/downsell')
    snaptr('track', 'CUSTOM_EVENT_1', {'downsell': 1});  
 
 if(window.location.href=='https://offer.thakea.com/downsell_bundle')
-   snaptr('track', 'CUSTOM_EVENT_1', {'downsell_bundle': 1});  
+   snaptr('track', 'CUSTOM_EVENT_1', {'downsellbundle': 1});  
 
 
-if(window.location.href=='https://offer.thakea.com/discount_only')
-   snaptr('track', 'CUSTOM_EVENT_1', {'discount_only': 1});  
+if(window.location.href=='https://offer.thakea.com/discountonly')
+   snaptr('track', 'CUSTOM_EVENT_1', {'discountonly': 1});  
