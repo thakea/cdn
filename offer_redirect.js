56758be8bba011e612ac47844bc3c1f1e913dee8
thakea <70716399+thakea@users.noreply.github.com>
2020-09-16 15:58:14 +0300

    Update offer_redirect.js

--- offer_redirect.js
+++ offer_redirect.js
@@ -1,18 +1,15 @@
-document.getElementById('m727').querySelectorAll(".btn")[0].addEventListener("click", function() {
-   console.log("m727 (offer1) was clicked");
-   snaptr('track', 'CUSTOM_EVENT_2', {'offer': 'offer1'});  
-   window.location.replace('https://thakea.com/XPmGqV');
-});
+if(window.location.href=='https://offer.thakea.com/upsell')
+   snaptr('track', 'CUSTOM_EVENT_1', {'upsell': 1});  
 
-document.getElementById('m731').querySelectorAll(".btn")[0].addEventListener("click", function() {
-   console.log("m731 (offer2) was clicked");
-   snaptr('track', 'CUSTOM_EVENT_2', {'offer': 'offer2'});  
-   window.location.replace('https://thakea.com/PlPwOG');
-});
+if(window.location.href=='https://offer.thakea.com/upsell_bundle')
+   snaptr('track', 'CUSTOM_EVENT_1', {'upsell_bundle': 1});  
+
+if(window.location.href=='https://offer.thakea.com/downsell')
+   snaptr('track', 'CUSTOM_EVENT_1', {'downsell': 1});  
+
+if(window.location.href=='https://offer.thakea.com/downsell_bundle')
+   snaptr('track', 'CUSTOM_EVENT_1', {'downsell_bundle': 1});  
 
 
-document.getElementById('m732').querySelectorAll(".btn")[0].addEventListener("click", function() {
-   console.log("m732 (offer3) was clicked");
-   snaptr('track', 'CUSTOM_EVENT_2', {'offer': 'offer3'});     
-   window.location.replace('https://thakea.com/rxWKqQ');
-});
+if(window.location.href=='https://offer.thakea.com/discount_only')
+   snaptr('track', 'CUSTOM_EVENT_1', {'discount_only': 1});  
